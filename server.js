90dae9143d022a02b72f24b354aa831412387f8c
Andrew McDonald <mcdonalda1993@users.noreply.github.com>
2016-03-15 22:37:51 +0000

    Add update url
    Add update url to make updating the server easier.

--- server.js
+++ server.js
@@ -5,6 +5,35 @@ app.get("/", function(req, res){
 	res.send("Hello World");
 });
 
+app.get("/update", function(req, res){
+	var exec = require('child_process').execSync;
+	var getCurrentHashCMD = 'git show-ref';
+	var currentHash;
+	var getLatestRemoteHashCMD = 'git ls-remote origin';
+	var latestRemoteHash;
+	var updateCMD = "git pull";
+
+	exec(getCurrentHashCMD, function(error, stdout, stderr) {
+  		if(!error){
+  			currentHash = stdout.split(" ")[0];
+  		}
+	});
+	
+	exec(getLatestRemoteHashCMD, function(error, stdout, stderr) {
+  		if(!error){
+  			latestRemoteHash = stdout.split(" ")[0];
+  		}
+	});
+	
+	if(currentHash != latestRemoteHash){
+		exec(updateCMD);
+		res.send("Was on: " + currentHash + "\r\n Updated to: " + latestRemoteHash);
+	} else{
+		res.send("Up to date. On revision: " + currentHash);
+	}
+	res.send("Hello World");
+});
+
 var server = app.listen(80, function(){
 	var host = server.address().address;
 	var port = server.address().port;
