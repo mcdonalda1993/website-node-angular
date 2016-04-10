fa5965cdad93c699c9a22d973a54e6b2f8f3fec9
Andrew McDonald <mcdonalda1993@gmail.com>
2016-04-10 01:10:00 +0100

    Rename templates folder to views

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 var app = express();
 app.use(express.static('public'));
-nunjucks.configure('templates', {
+nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
