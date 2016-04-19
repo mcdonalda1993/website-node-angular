1271ef021a0ff7a3d424030d27914054b31fb7e0
Andrew McDonald <mcdonalda1993@gmail.com>
2016-04-19 23:21:27 +0100

    Update server.js to utilize lets-encrypt

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
-var nunjucks =  require('nunjucks');
-var express = require("express");
+var http = require('http');
+var https = require('spdy');
+// NOTE: you could use the old https module if for some reason you don't want to support modern browsers
 
-var app = express();
-app.use(express.static('public'));
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-});
+function redirectHttp() {
+  http.createServer(LEX.createAcmeResponder(lex, function redirectHttps(req, res) {
+    res.setHeader('Location', 'https://' + req.headers.host + req.url);
+    res.statusCode = 302;
+    res.end('<!-- Hello Developer Person! Please use HTTPS instead -->');
+  })).listen(80);
+}
 
-app.get("/update", function(req, res){
-	var context = {};
-	
-	var exec = require('child_process').execSync;
-	var getCurrentHashCMD = 'git show-ref';
-	var getLatestRemoteHashCMD = 'git ls-remote origin';
-	var updateCMD = "git pull";
+function serveHttps() {
+	var nunjucks =  require('nunjucks');
+	var express = require("express");
 
-	context.current = exec(getCurrentHashCMD).toString().split(" ")[0];
-	
-	context.latest = exec(getLatestRemoteHashCMD).toString().split("	")[0];
-	
-	if(context.current != context.latest){
-		exec(updateCMD);
-	}
-	
-	res.render("update.html", context)
-});
+	var app = express();
+	app.use(express.static('public'));
+	nunjucks.configure('views', {
+		autoescape: true,
+		express: app
+	});
 
-app.get("/", function(req, res){
-	res.render("index.html");
-});
+	app.get("/update", function(req, res){
+		var context = {};
+		
+		var exec = require('child_process').execSync;
+		var getCurrentHashCMD = 'git show-ref';
+		var getLatestRemoteHashCMD = 'git ls-remote origin';
+		var updateCMD = "git pull";
 
-var server = app.listen(80, function(){
-	var host = server.address().address;
-	var port = server.address().port;
+		context.current = exec(getCurrentHashCMD).toString().split(" ")[0];
+		
+		context.latest = exec(getLatestRemoteHashCMD).toString().split("	")[0];
+		
+		if(context.current != context.latest){
+			exec(updateCMD);
+		}
+		
+		res.render("update.html", context)
+	});
 
-	console.log("Server listening at http://%s:%s", host, port);
-});
+	app.get("/", function(req, res){
+		res.render("index.html");
+	});
+
+  https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app)).listen(443);
+}
+
+redirectHttp();
+serveHttps();
